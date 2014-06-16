0edf5c903ca0bdaf1b0ea58ad592e5c679572d57
geralHenriquez2011 <geral.henriquez2011@gmail.com>
2014-06-16 13:12:33 -0400

    carga inicial
    se configura spring y ftl

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,13 @@
+package service;
+
+import java.util.List;
+
+import model.User;
+
+public class UserService {
+	/*
+	 	public void saveUser(User user);
+	    public void updateUser(User user);
+	    public void deleteUser(String id);
+	    public List<User> getAllUser();*/
+}
